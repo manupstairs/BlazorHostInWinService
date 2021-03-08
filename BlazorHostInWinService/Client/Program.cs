9811d9f4f1338086ccd16bcb26e72a602c6a906f
xingzheng <xingzheng@hotmail.com>
2021-03-08 21:42:20 +0800

    Add host project

--- BlazorHostInWinService/Client/Program.cs
+++ BlazorHostInWinService/Client/Program.cs
@@ -8,7 +8,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BlazorHostInWinService
+namespace BlazorHostInWinService.Client
 {
     public class Program
     {
